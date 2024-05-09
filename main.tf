a28e3b8722adda684ee7c98989a25e35d0dd5533
Anu <143781093+anudhiman18@users.noreply.github.com>
2024-05-10 00:12:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    newrelic = {
+      source = "newrelic/newrelic"
+      version = "3.34.1"
+    }
+  }
+}
+
 provider "newrelic" {
   api_key    = var.NEW_RELIC_API_KEY
   account_id = var.NEW_RELIC_ACCOUNT_ID
