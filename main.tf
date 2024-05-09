73cb575efe03a9ef4c7176d468c61474cba3bd6f
Anu <143781093+anudhiman18@users.noreply.github.com>
2024-05-10 00:53:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "newrelic" {
-  api_key    = var.NEW_RELIC_API_KEY
-  account_id = var.NEW_RELIC_ACCOUNT_ID
+  NEW_RELIC_API_KEY    = var.NEW_RELIC_API_KEY
+  NEW_RELIC_ACCOUNT_ID = var.NEW_RELIC_ACCOUNT_ID
 }
 
 resource "newrelic_alert_policy" "policy" {
