7352c7a73fe0e85726013e6ae1e175d6d9763acd
Anu <143781093+anudhiman18@users.noreply.github.com>
2024-05-10 00:02:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "newrelic" {
+  api_key    = var.NEW_RELIC_API_KEY
+  account_id = var.NEW_RELIC_ACCOUNT_ID
+}
+
 resource "newrelic_alert_policy" "policy" {
   for_each = toset(var.policy)
 
