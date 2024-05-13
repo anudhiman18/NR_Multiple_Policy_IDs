1c2407067c847a64588ba925b76659c74e1fda87
Anu <143781093+anudhiman18@users.noreply.github.com>
2024-05-13 13:54:46 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    newrelic = {
-      source = "newrelic/newrelic"
-      version = "3.34.1"
-    }
-  }
-}
-
-provider "newrelic" {
-  NEW_RELIC_API_KEY    = var.NEW_RELIC_API_KEY
-  NEW_RELIC_ACCOUNT_ID = var.NEW_RELIC_ACCOUNT_ID
-}
-
 resource "newrelic_alert_policy" "policy" {
   for_each = toset(var.policy)
 
