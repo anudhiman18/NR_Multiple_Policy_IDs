8241058e37c77845ac4386e5e73c546b8d8ae0dd
Anu <143781093+anudhiman18@users.noreply.github.com>
2024-05-16 16:36:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,15 +2,9 @@ variable "policy" {
     type = set(string)
 }
 
-variable "NEW_RELIC_API_KEY" {
-  description = "API key for New Relic"
-
-}
+variable "NEW_RELIC_API_KEY" {}
  
-variable "NEW_RELIC_ACCOUNT_ID" {
-  description = "Account ID for New Relic"
-
-}
+variable "NEW_RELIC_ACCOUNT_ID" {}
 
 # variable "policy_name" {
 #   type = set(string)
