448240fdeea011c806f64f338330415238291e80
Anu <143781093+anudhiman18@users.noreply.github.com>
2024-05-16 15:18:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
+data "github_actions_secret" "new_relic_account_id" {
+  repository = "NR_Multiple_Policy_IDs"
+  secret_name = "NEW_RELIC_ACCOUNT_ID"
+}
+
 provider "newrelic" {
   api_key    = var.NEW_RELIC_API_KEY
   account_id = data.github_actions_secret.NEW_RELIC_ACCOUNT_ID.value
