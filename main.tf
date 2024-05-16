a671a1f6da188cae12dfb2aad2f1a635d97b293c
Anu <143781093+anudhiman18@users.noreply.github.com>
2024-05-16 15:20:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@ resource "newrelic_alert_policy" "policy" {
 
   name = each.value
 }
+resource "github_actions_secret" "new_relic_account_id" {
+  repository = "NR_Multiple_Policy_IDs"
+  secret_name = "NEW_RELIC_ACCOUNT_ID"
+}
