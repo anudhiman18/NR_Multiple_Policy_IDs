a0637051208d9d039be8fbc8b396ca682888949a
Anu <143781093+anudhiman18@users.noreply.github.com>
2024-05-16 16:22:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,21 +8,9 @@ terraform {
 }
 
 
-# provider "newrelic" {
-#  api_key    = var.NEW_RELIC_API_KEY.${{ secrets.NEW_RELIC_API_KEY }}
-#  account_id = var.NEW_RELIC_ACCOUNT_ID.${{ secrets.NEW_RELIC_ACCOUNT_ID }}
-# }
-
-
-data "github_actions_secret" "newrelic_api_key" {
-  secret_name = "NEW_RELIC_API_KEY"
-}
-
-data "github_actions_secret" "newrelic_account_id" {
-  secret_name = "NEW_RELIC_ACCOUNT_ID"
-}
-
 provider "newrelic" {
-  api_key    = data.github_actions_secret.newrelic_api_key.value
-  account_id = data.github_actions_secret.newrelic_account_id.value
+api_key    = var.NEW_RELIC_API_KEY
+account_id = var.NEW_RELIC_ACCOUNT_ID
+}
+
 }
